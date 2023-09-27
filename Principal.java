1e9b69865d525e3d640fdbb7f3577e00bb14a102
Salim Ferez Mendez <123603908+salfermen@users.noreply.github.com>
2023-09-27 18:38:33 -0500

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,14 @@
+public class computador_java {
+    /**
+     * Primer método que se ejecuta en mi aplicación
+     * @param args
+     *
+     * Complejidad temporal: O(1) Complejidad Constante
+     */
+
+    public static void main(String[] args) {
+        computador Info = new computador("Procesador Intel o AMD","Grafica Nvidia GTX/RTX o AMD Radeon","4,8,16 O 32GB","250/500GB o 1/2 TB","450W,650W,800W o 1000W");
+        Info.infoComputador();
+
+    }
+}
